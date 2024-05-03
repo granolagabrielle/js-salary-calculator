df8b27e17200d6f3645344fa6dfedb26171a058b
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-05-03 17:08:46 -0500

    Add functions to render employees and calculate cost

--- client.js
+++ client.js
@@ -6,14 +6,67 @@ function onReady() {
   console.log('Javascript is working');
 }
 
+// array to store employees
+
+const employeeList = [];
+
 // function to add new employee to table
 
 function addEmployee(event) {
+  // prevent default behavior
   event.preventDefault();
-  const submitButton = event.target;
+  // test
   console.log('add employee button was clicked');
+  // reference inputs on DOM
+  const firstName = document.getElementById('first-name');
+  const lastName = document.getElementById('last-name');
+  const employeeId = document.getElementById('employee-id');
+  const jobTitle = document.getElementById('job-title');
+  const annualSalary = document.getElementById('annual-salary');
+  // create new object with values from form
+  const employee = {
+    first: firstName.value,
+    last: lastName.value,
+    id: employeeId.value,
+    title: jobTitle.value,
+    salary: annualSalary.value,
+  };
+  // test new employee
+  console.log(employee);
+  // push into array
+  employeeList.push(employee);
+  // reset form
+  document.getElementById('form').reset();
+  renderEmployees();
+  calculateCost();
+}
+
+// function to render employee list
+function renderEmployees() {
+  const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = '';
+  for (let i = 0; i < employeeList.length; i++) {
+    tableBody.innerHTML += `
+        <tr>
+            <td>${employeeList[i].first}</td>
+            <td>${employeeList[i].last}</td>
+            <td>${employeeList[i].id}</td>
+            <td>${employeeList[i].title}</td>
+            <td>$${employeeList[i].salary}</td>
+            <td><button id="delete-button" onclick="removeEmployee(event)"</button></td>
+        </tr>    
+        `;
+  }
 }
 
 // function to delete employee from table
 
 // calculate monthly salary costs
+function calculateCost() {
+  let total = 0;
+  for (const person of employeeList) {
+    total += Math.ceil(person.salary / 12);
+  }
+  const salaryTotal = document.getElementById('cost');
+  salaryTotal.innerHTML = `$${total}`;
+}
