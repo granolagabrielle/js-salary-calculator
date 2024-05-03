6f7ef5130aaca95d5a793c9b48097858854d5a90
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-05-03 15:42:03 -0500

    Add employee button is connected

--- client.js
+++ client.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+// on ready function
+
+onReady();
+
+function onReady() {
+  console.log('Javascript is working');
+}
+
+// function to add new employee to table
+
+function addEmployee(event) {
+  event.preventDefault();
+  const submitButton = event.target;
+  console.log('add employee button was clicked');
+}
+
+// function to delete employee from table
+
+// calculate monthly salary costs
